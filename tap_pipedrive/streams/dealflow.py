b8e294212e88829f7bbb91e8b991c5a355e0790b
sipilat <tuomas.sipila@quickbi.io>
2022-02-15 15:10:33 +0200

    feat: Add user, value and status field keys to dealflow

--- tap_pipedrive/streams/dealflow.py
+++ tap_pipedrive/streams/dealflow.py
@@ -15,7 +15,9 @@ class DealStageChangeStream(PipedriveIterStream):
     def process_row(self, row):
         # grab only rows that are dealChange objects with changes in add_time or stage_id
         if row['object'] == 'dealChange':
-            if row['data']['field_key'] == 'add_time' or row['data']['field_key'] == 'stage_id':
+            if row['data']['field_key'] == 'add_time' or row['data']['field_key'] == 'stage_id' \
+                or row['data']['field_key'] == 'value' or row['data']['field_key'] == 'status' \
+                or row['data']['field_key'] == 'user_id':
                 return row['data']
 
     def update_endpoint(self, deal_id):
